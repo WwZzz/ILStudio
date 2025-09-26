250da280bb073be4170a235ae12dd8273b3c735c
wwzzz <zwang@stu.xmu.edu.cn>
2025-09-26 20:15:40 +0800

    remove testing scripts

--- test_d4rl.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-import gymnasium_robotics
-
-gym.register_envs(gymnasium_robotics)
-
-env = gym.make("FetchReach-v4")
-env.reset()
-obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
-
