cb44341c7ace8969c9c95412b1e837749c65d811
wwzzz <zwang@stu.xmu.edu.cn>
2025-10-08 03:57:45 +0800

    remove files of openvla

--- policy/openvla/__init__.py
+++ policy/openvla/__init__.py
@@ -7,13 +7,10 @@ from .simple_openvla import (
     get_data_processor,
     SimpleOpenVLAProcessor as OpenVLAProcessor
 )
-from .trainer import OpenVLATrainer, create_openvla_trainer
 
 __all__ = [
     'OpenVLAPolicy',
     'OpenVLAPolicyConfig', 
-    'OpenVLATrainer',
-    'create_openvla_trainer',
     'load_model',
     'get_data_collator',
     'get_data_processor',
