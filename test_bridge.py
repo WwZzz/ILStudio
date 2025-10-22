acb6478c52d30df161cc1a7ca89311af33132f2e
wwzzz <zwang@stu.xmu.edu.cn>
2025-10-22 13:57:51 +0800

    remove unnecessary file

--- test_bridge.py
+++
@@ -1,7 +0,0 @@
-import tensorflow_datasets as tfds 
-from policy.openvla.prismatic.vla.datasets.datasets import RLDSDataset
-
-if __name__=='__main__':
-    # ds = tfds.load('bridge_orig', split='train', data_dir='/inspire/hdd/project/robot-action/wangzheng-240308120196/data/bridge_data_v2')
-    data = RLDSDataset(data_root_dir='/inspire/hdd/project/robot-action/wangzheng-240308120196/data/bridge_data_v2', data_mix='bridge', batch_transform=lambda x:x, resize_resolution=[256, 256])
-    print('ok')
